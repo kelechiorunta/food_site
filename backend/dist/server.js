1e149e7b5b7ced8eceeca8ea616eb751da2d99b2
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-13 00:12:44 +0100

    feat: initialize  server setup

---
+++ backend/dist/server.js
@@ -0,0 +1,63 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const express_session_1 = __importDefault(require("express-session"));
+const cors_1 = __importDefault(require("cors"));
+const connect_mongodb_session_1 = __importDefault(require("connect-mongodb-session"));
+const app = (0, express_1.default)();
+const DEV_PORT = 3110;
+const PORT = process.env.PORT || DEV_PORT;
+const limiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
+const corsOption = {
+    origin: (origin, callback) => {
+        if (allowedOrigins?.includes(origin) || !origin) {
+            return callback(null, true);
+        }
+        else {
+            return callback(false, 'Domain not supported');
+        }
+    },
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+};
+const MongoDBStore = (0, connect_mongodb_session_1.default)(express_session_1.default);
+const mongo_db_uri = process.env.MONGO_URI;
+const store = new MongoDBStore({
+    uri: mongo_db_uri,
+    collection: 'sessions',
+    expires: 1000 * 60 * 60 * 24 * 7
+});
+console.log('SECRET', process.env.SESSION_SECRET);
+const sessionOptions = {
+    name: 'secret_auth_session',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    store: store
+};
+// Enable trust proxy/ reverse proxy for secure cookies in session during production
+// through X-Forwarded-Proto header set to true/1
+app.set('trust-proxy', 1);
+app.use((0, cors_1.default)(corsOption));
+app.use((0, express_session_1.default)(sessionOptions));
+app.use((0, morgan_1.default)('dev'));
+app.use(limiter);
+app.listen(() => {
+    console.log(`Server is running on port ${PORT}`);
+});
